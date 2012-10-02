04c52a4a8d5dddcbb5cdb724caeb841b5cab83fc
iluuuk <iluuuk@hotmail.com>
2012-10-02 18:42:02 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      #flash.now[:error] = "Errors prohibited this user from being saved"
       render 'new'
     end
   end
